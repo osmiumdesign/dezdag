162deb0e59b0ef88c1fbbe2e61d7148ec9f1c833
Osmium <dagtecinc@gmail.com>
2019-10-16 18:12:53 +0300

    new project

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    cellSelector: '.carousel-item',
+    prevNextButtons: false,
+    pageDots: false,
+    groupCells: true,
+    groupCells: 3
+});
+
+var elem = document.querySelector('.carousel-2');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    cellSelector: '.carousel-item-2',
+    prevNextButtons: false,
+    pageDots: false,
+    groupCells: true,
+    groupCells: 3
+});
+
+var elem = document.querySelector('.carousel-3');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    cellSelector: '.carousel-item-3',
+    prevNextButtons: false,
+    pageDots: false,
+    groupCells: true,
+    groupCells: 3
+});
+
+
+let contact = document.getElementById('contacts');
+let ctcbtn = document.getElementById('showcont');
+ctcbtn.onclick = function (event) {
+    contact.classList.toggle("contacts-open");
+    event.preventDefault();
+};
